4cd1db6e412b8dcd84d7e0dcb92da78d85cc3981
Ogbeide Godstime Osemenkhian <70475985+gtogbes@users.noreply.github.com>
2022-05-19 16:10:27 -0500

    updated tag conflict

--- eks.tf
+++ eks.tf
@@ -38,12 +38,12 @@ module "vpc" {
   single_nat_gateway     = true
   enable_dns_hostnames   = true
   
-  public_subnets_tags = {
+  public_subnet_tags = {
     "kubernetes.io/cluster/${local.cluster_name}" = "shared"
     "kubernetes.io/role/elb"                      = "1"
   }
   
-  private_subnets_tags = {
+  private_subnet_tags = {
     "kubernetes.io/cluster/${local.cluster_name}" = "shared"
     "kubernetes.io/role/internal-elb"             = "1"
   }
