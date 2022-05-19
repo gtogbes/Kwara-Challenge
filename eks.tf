e88395408f1f1be3763fc8e33149d497fa53270c
Ogbeide Godstime Osemenkhian <70475985+gtogbes@users.noreply.github.com>
2022-05-19 14:26:29 -0500

    DevOps
    added eks.tf

--- eks.tf
+++ eks.tf
@@ -20,57 +20,66 @@ provider "kubernetes" {
 data "aws_availabiltity_zones" "available" {
 }
   
+## Cluster Names
+locals {
+  cluster_name = "kwara-cluster"
+}
   
+modules "vpc" {
+  source   = "terraform-aws-modules/vpc/aws"
+  version  = "2.58.0"
   
+  name                   = "k8s-vpc"
+  cidr                   = "172.16.0.0/16"
+  azs                    = data.aws_availability_zones.available.names
+  private_subnets        = ["172.16.1.0/24", "172.16.3.0/24"]
+  public_subnets         = ["172.16.4.0/24", "172.16.5.0/24"]
+  enable_nat_gateway     = true
+  single_nat_gateway     = true
+  enable_dns_hostnames   = true
   
-  to be continued
-module "vpc" {
-  source = "terraform-aws-modules/vpc/aws"
-
-  name = "${var.environment}-${var.owner}-${random_id.id.hex}"
-  cidr = var.vpc_cidr
-
-  azs             = var.azs
-  private_subnets = var.private_subnets
-  public_subnets  = var.public_subnets
-
-  enable_nat_gateway = var.vpc_enable_nat_gateway
-  single_nat_gateway = var.vpc_single_nat_gateway
-
-  tags = {
-    Terraform   = "true"
-    Environment = terraform.workspace
+  public_subnets_tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+    "kubernetes.io/role/elb"                      = "1"
+  }
+  
+  private_subnets_tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+    "kubernetes.io/role/internal-elb"             = "1"
   }
 }
 
-
-module "eks" {
-  source  = "terraform-aws-modules/eks/aws"
-  version = "17.16.0"
-
-  depends_on = [
-    module.vpc,
-    module.efs
-  ]
-
-  cluster_name    = "${var.environment}-${var.owner}-${random_id.id.hex}"
-  cluster_version = "1.21"
-  subnets         = concat(module.vpc.public_subnets, module.vpc.private_subnets)
-  vpc_id          = module.vpc.vpc_id
-  manage_aws_auth = false
-  enable_irsa     = true
-  workers_group_defaults = {
-    instance_type        = "t2.micro"   #AMD based processor
-    subnets              = module.vpc.private_subnets
-    asg_desired_capacity = 1
-    asg_min_size         = 2
-    asg_max_size         = 3
-  }
+modules "eks" {
+  source   = "terraform-aws-modules/eks/aws"
+  version  = "12.2.0"
   
-  node_groups = {
-    worker = {
-      version = "1.21"
-      capacity_type = "SPOT"  #using AWS SPOT instance for testing, should change when it comes to production
+  cluster_name     = "${local.cluster_name}"
+  cluster_version  = "1.17"
+  subnets          = module.vpc.private_subnets
+  
+  vpc_id = module.vpc.vpc_id
+   
+  node_group = {
+    first = {
+      desired_capacity = 1
+      max_capacity     = 3
+      min_capacity     = 1
+      
+      instance_type = "t2.micro"
     }
   }
+  
+  write_kubeconfig   = true
+  config_output_path = "./"
+}
+  
+## Get bucket
+data "aws_s3_bucket" "getbucket" {
+  bucket = "kwara-bucket"
+}
+## S3 bucket object upload
+resource "aws_s3_bucket_object" "object" {
+  bucket = data.aws_s3_bucket.getbucket.id
+  key    = module.eks.kubeconfig_filename
+  source = "./${module.eks.kubeconfig_filename}"
 }
