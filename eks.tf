546f0c010226b44cd724ac074e1b81bd21cb88e2
Ogbeide Godstime Osemenkhian <70475985+gtogbes@users.noreply.github.com>
2022-05-19 12:59:45 -0500

    started tf eks.tf

--- eks.tf
+++ eks.tf
@@ -1,3 +1,49 @@
+##EKS Cluster id
+data "aws_eks_cluster" "cluster" {
+  name = module.eks.cluster_id
+}
+  
+##EKS cluster auth
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_id
+}
+##Kubernetes provider 
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+  load_config_file       = false
+  version                = "~> 1.11"
+}  
+
+## Availability zones 
+data "aws_availabiltity_zones" "available" {
+}
+  
+  
+  
+  
+  to be continued
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "${var.environment}-${var.owner}-${random_id.id.hex}"
+  cidr = var.vpc_cidr
+
+  azs             = var.azs
+  private_subnets = var.private_subnets
+  public_subnets  = var.public_subnets
+
+  enable_nat_gateway = var.vpc_enable_nat_gateway
+  single_nat_gateway = var.vpc_single_nat_gateway
+
+  tags = {
+    Terraform   = "true"
+    Environment = terraform.workspace
+  }
+}
+
+
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "17.16.0"
