1c1a1b9f7c25d0ec80b30120c2e2897543e98586
Ogbeide Godstime Osemenkhian <70475985+gtogbes@users.noreply.github.com>
2022-05-20 06:52:08 +0100

    Update eks.tf

--- eks.tf
+++ eks.tf
@@ -13,7 +13,7 @@ provider "kubernetes" {
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
   token                  = data.aws_eks_cluster_auth.cluster.token
   load_config_file       = false
-  version                = "~> 1.11.0"
+  version                = "~> 0.12"
 }  
 
 ## Availability zones 
